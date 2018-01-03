5636a221255e5c8c3263f6741884127322fa9196
wallyfaye <wally.faye@gmail.com>
2018-01-03 16:51:24 -0500

    class scaffolding

---
+++ foo-bar/src/Baz.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Foo\Bar;
+
+	class Baz{
+
+		public $foo_bar = 123;
+
+		public function __construct(){
+		}
+
+	}
---
+++ foo-bar/src/Qux/Quux.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Foo\Bar\Qux;
+
+	class Quux{
+
+		public $foo_bar = 456;
+
+		public function __construct(){
+		}
+
+	}
