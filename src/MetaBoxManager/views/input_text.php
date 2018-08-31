a87c7d5ec95ada4a184723f858ae1582caf6d7e4
Wally Faye <wally.faye@gmail.com>
2018-08-31 17:20:20 -0400

    meta box save value

--- src/MetaBoxManager/views/input_text.php
+++ src/MetaBoxManager/views/input_text.php
@@ -1,1 +1,6 @@
-<input type="text" id="<?php echo $field_slug; ?>" name="<?php echo $field_slug; ?>" value="<?php echo $field_value; ?>" >
+<input 
+  type="text" 
+  id="<?php echo $field_slug; ?>" 
+  name="<?php echo $field_slug; ?>" 
+  value="<?php echo $field_value; ?>" 
+/>
